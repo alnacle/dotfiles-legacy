edce36073581b1d081e478450026de7f6cf0013f
Alvaro Navarro <alvaro.navarro@amadeus.com>
2018-03-04 15:07:23 +0100

    Update install script. Remove bundle autoinstall from vimrc

--- zsh/darwin.zsh
+++ zsh/darwin.zsh
@@ -1,3 +1,5 @@
+# brew install zsh-completion zsh-syntax-highlighting zsh-autosuggestions thefuck
+
 if [ -f /usr/local/bin/exa ]; then
     alias ls='/usr/local/bin/exa'
 fi
@@ -6,12 +8,17 @@ if [ -f /usr/local/bin/ccat ]; then
     alias ls='/usr/local/bin/ccat'
 fi
 
+export HOMEBREW_HOME=/usr/local/share
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-eval "$(thefuck --alias)"
 
-# zsh-completions
-[ -d /usr/local/share/zsh-completions ] && fpath=(/usr/local/share/zsh-completions $fpath)
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+eval "$(thefuck --alias)"
 
 # fzf 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
+[ -d $HOMEBREW_HOME/zsh-completions ] && fpath=($HOMEBREW_HOME/zsh-completions $fpath)
+[ -d $HOMEBREW_HOME/zsh-syntax-highlighting ] && source $HOMEBREW_HOME/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+[ -d $HOMEBREW_HOME/zsh-autosuggestions ] && source $HOMEBREW_HOME/zsh-autosuggestions/zsh-autosuggestions.zsh
